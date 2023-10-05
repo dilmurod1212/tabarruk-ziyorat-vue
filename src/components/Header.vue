0697af4bc6176af8f5ab4122b031a2674b384af7
dilmurod <dilmurod121202@gmail.com>
2023-10-05 14:50:56 +0500

    fix bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="bg-hero-pattern bg-center bg-cover h-screen">
+    <header class="bg-hero-pattern bg-center bg-cover h-screen max-md:h-auto">
       <div class="container">
         <div class="flex justify-between items-center">
           <div class="logo">
@@ -10,7 +10,7 @@
             <Navbar />
             <LanguageSwitcher />
             <SearchPanel @click="showSearchBar" />
-            <i class="fas fa-bars text-white open hidden max-md:block"></i>
+            <i class="fas fa-bars text-white open hidden max-md:block text-2xl"></i>
           </nav>
         </div>
         <div v-show="store.searchBar">
@@ -19,12 +19,14 @@
         <div class="text-center h-[75vh] flex flex-col items-center justify-center text-white">
           <p><i class="fa-solid fa-location-dot mx-1"></i>Samarkand, Uzbekistan</p>
           <h1 class="text-6xl max-md:text-4xl capitalize font-playfair">registan square</h1>
-          <p class="descr w-4/6 max-md:w-full max-md:text-sm my-3">
+          <p class="descr w-4/6 max-md:w-4/5 max-md:text-sm my-3">
             Stunning showcase of Islamic architecture in Samarkand, Uzbekistan, with three ornate
             madrasas adorned in blue tiles and calligraphic inscriptions, dating back to the 15th
             century.
           </p>
-          <Button type="button" btnClass=""  variant="red">Explore <i class="fas fa-arrow-right"></i></Button>
+          <Button type="button" btnClass="" variant="red"
+            >Explore <i class="fas fa-arrow-right"></i
+          ></Button>
         </div>
       </div>
     </header>
