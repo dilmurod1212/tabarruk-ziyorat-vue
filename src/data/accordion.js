2304071cd1e715bef50449cae154a2428ac14a0c
dilmurod <dilmurod121202@gmail.com>
2023-10-09 13:30:25 +0500

    ok

---
+++ src/data/accordion.js
@@ -0,0 +1,50 @@
+export const Accordion = [
+  {
+    id: 1,
+    show: false,
+    icon: '/images/accordion-icon/flag.svg',
+    title: 'What is the goal of this project? ',
+    descr:
+      'In Organization of Turkic states 7 countries: Uzbekistan, Kazakhstan, Kirgizstan, Turkey, Azerbaijan, Turkmenistan'
+  },
+  {
+    id: 2,
+    show: true,
+    icon: '/images/accordion-icon/bolt.svg',
+    title: 'How many countries in Organization of Turkic states?',
+    descr:
+      'In Organization of Turkic states 7 countries: Uzbekistan, Kazakhstan, Kirgizstan, Turkey, Azerbaijan, Turkmenistan'
+  },
+  {
+    id: 3,
+    show: false,
+    icon: '/images/accordion-icon/flag.svg',
+    title: 'What is the goal of this project? ',
+    descr:
+      'In Organization of Turkic states 7 countries: Uzbekistan, Kazakhstan, Kirgizstan, Turkey, Azerbaijan, Turkmenistan'
+  },
+  {
+    id: 4,
+    show: false,
+    icon: '/images/accordion-icon/location-plus.svg',
+    title: 'How many countries in Organization of Turkic states?',
+    descr:
+      'In Organization of Turkic states 7 countries: Uzbekistan, Kazakhstan, Kirgizstan, Turkey, Azerbaijan, Turkmenistan'
+  },
+  {
+    id: 5,
+    show: false,
+    icon: '/images/accordion-icon/flag.svg',
+    title: 'How many countries in Organization of Turkic states?',
+    descr:
+      'In Organization of Turkic states 7 countries: Uzbekistan, Kazakhstan, Kirgizstan, Turkey, Azerbaijan, Turkmenistan'
+  },
+  {
+    id: 6,
+    show: false,
+    icon: '/images/accordion-icon/user-heart-alt.svg',
+    title: 'How many countries in Organization of Turkic states?',
+    descr:
+      'In Organization of Turkic states 7 countries: Uzbekistan, Kazakhstan, Kirgizstan, Turkey, Azerbaijan, Turkmenistan'
+  }
+]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,7 +40,7 @@
 import { ref, reactive, onMounted } from 'vue'
 import Header from '@/components/Header.vue'
 import About from '@/components/About/About.vue'
-import CountrySlider from '@/components/CountrySlider.vue'
+// import CountrySlider from '@/components/CountrySlider.vue'
 import CCountries from '@/components/Countries/CCountries.vue'
 import CCountryInfo from '@/components/CardInfo/CCountryInfo.vue'
 import CReligions from '@/components/Religions/CReligions.vue'
