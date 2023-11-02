4ed9fa1a1d95448239436064c569354e4227d08b
dilmurod <dilmurod121202@gmail.com>
2023-11-02 18:56:12 +0500

    comp

---
+++ src/data/dropdown.js
@@ -0,0 +1,52 @@
+export const dropdownCountries = [
+  {
+    id: 1,
+    img: '/images/dropdown-flag-icons/Uzbekistan.svg',
+    title: 'Uzbekistan',
+    descr: 32
+  },
+  { id: 1, img: '/images/dropdown-flag-icons/Turkiye.svg', title: 'Türkiye', descr: 27 },
+  {
+    id: 2,
+    img: '/images/dropdown-flag-icons/Turkmenistan.svg',
+    title: 'Turkmenistan',
+    descr: 27
+  },
+  {
+    id: 3,
+    img: '/images/dropdown-flag-icons/Azerbaijan.svg',
+    title: 'Azerbaijan',
+    descr: 12
+  },
+  { id: 4, img: '/images/dropdown-flag-icons//Hungary.svg', title: 'Hungary', descr: 19 },
+  {
+    id: 5,
+    img: '/images/dropdown-flag-icons/Kazakhstan.svg',
+    title: 'Kazakhstan',
+    descr: 21
+  },
+  {
+    id: 6,
+    img: '/images/dropdown-flag-icons/Kyrgyzstan.svg',
+    title: 'Kyrgyzstan',
+    descr: 9
+  }
+]
+
+export const religions = [
+  { img: '/images/religions/Islam (1).svg', title: 'Islam', descr: '32' },
+  { img: '/images/religions/Islam.svg', title: 'Christianity', descr: '24' },
+  { img: '/images/religions/Islam (2).svg', title: 'Buddism', descr: '19' }
+]
+
+export const languageSwitcherData = [
+  { id: 1, title: 'Русский', img: '/images/language-icons/Russia.svg' },
+  { id: 2, title: 'English', img: '/images/language-icons/UK.svg' },
+  { id: 3, title: 'O’zbekcha', img: '/images/language-icons/Uzbekistan.svg' },
+  { id: 4, title: 'Қазақша', img: '/images/language-icons/Kazakhstan.svg' },
+  { id: 5, title: 'Қыргызча', img: '/images/language-icons/Krgyzstan.svg' },
+  { id: 6, title: 'Türk', img: '/images/language-icons/Turkiye.svg' },
+  { id: 7, title: 'Türkmen', img: '/images/language-icons/Turkmenistan.svg' },
+  { id: 8, title: 'Azərbaycan', img: '/images/language-icons/Azerbaijan.svg' },
+  { id: 9, title: 'Magyar', img: '/images/language-icons/Hungary.svg' }
+]
